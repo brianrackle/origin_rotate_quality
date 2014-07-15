c317a965a16e934e7c6a853a59100d7b889cd924
brianrackle <brianrackle@hotmail.com>
2014-07-15 14:11:42 -0700

    adding markdown output

--- console/console.cpp
+++ console/console.cpp
@@ -8,7 +8,7 @@
 #include <vector>
 #include <chrono>
 #include <functional>
-#include <iostream>
+#include <fstream>
 
 struct Coord2D
 {
@@ -22,36 +22,47 @@ struct Line
 	Coord2D from;
 };
 
-namespace utility
+
+//Calculate the angle of the vector to the horizontal.
+__forceinline double_t DotAngleH(Coord2D const& vector)
 {
-	//Calculate the angle of the vector to the horizontal.
-	__forceinline double_t DotAngleH(Coord2D const& vector)
-	{
-		return vector.x / std::hypot(vector.x, vector.y);
-	}
-
-	//Calculate the angle of the vector to the horizontal using atan2.
-	__forceinline double_t TrigAngleH(Coord2D const& line)
-	{
-		return 	std::atan2(line.y, line.x);
-	}
-
-	//Apply rotation matrix to the vector using radians
-	__forceinline void Rotate(Coord2D & vector, double_t radians)
-	{
-		double_t s = std::sin(radians);
-		double_t c = std::cos(radians);
-
-		vector = { vector.x * c - vector.y * s, vector.x * s + vector.y * c };
-	}
-
-	//Increment lhs by rhs
-	__forceinline void Increment(double_t & lhs, double_t const& rhs)
-	{
-		lhs += rhs;
-	}
+	return vector.x / std::hypot(vector.x, vector.y);
 }
-#define RADIAN_FRACTION 1.0e-8
+
+//Calculate the angle of the vector to the horizontal using atan2.
+__forceinline double_t TrigAngleH(Coord2D const& line)
+{
+	return 	std::atan2(line.y, line.x);
+}
+
+//Apply rotation matrix to the vector using radians
+__forceinline void Rotate(Coord2D & vector, double_t radians)
+{
+	double_t s = std::sin(radians);
+	double_t c = std::cos(radians);
+
+	vector = { vector.x * c - vector.y * s, vector.x * s + vector.y * c };
+}
+
+//Increment lhs by rhs
+__forceinline void Increment(double_t & lhs, double_t const& rhs)
+{
+	lhs += rhs;
+}
+
+__forceinline std::string dts(double_t d)
+{
+	char buf[50];
+	sprintf_s(buf, "%.16f", d);
+	return buf;
+}
+
+__forceinline std::string cts(Coord2D const& vector)
+{
+	return dts(vector.x) + " , " + dts(vector.y);
+}
+
+#define RADIAN_FRACTION 1.0e-2
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -68,10 +79,29 @@ int _tmain(int argc, _TCHAR* argv[])
 	//	-populate an std::vector with the values and then iterate. Excessive memory usage
 	//http://stackoverflow.com/questions/3162643/proper-trigonometry-for-rotating-a-point-around-the-origin
 
-	//THIS WAY SUCKS because error builds up in vector.
-	//vs
-	//vector being recalculated each iteration
+	std::ofstream outfile("results.markdown");
+	//link TOC so we can put a link back at the beginning of each header
+	outfile << "[TOC]\n";
+	
+	auto tableHeader = [](std::ofstream & file, char const* tableName)
 	{
+		file << "## " << tableName << " Results\n"
+			<< "| Vector | Radians |\n"
+			<< "|:------ | -------:|\n";
+	};
+
+	//rotate because error builds up in vector.
+	//vs
+	//vector being recalculated from polar on each iteration
+	{
+		double count;
+		double sum;
+		double mean;
+		double minDelta;
+		double maxDelta;
+
+		//could also store delta of every-1 angle and make a 1d heatmap out of it
+
 		auto rotationIterator =
 			[](std::function<void(Coord2D const&)> func)
 			-> std::chrono::nanoseconds
@@ -81,18 +111,30 @@ int _tmain(int argc, _TCHAR* argv[])
 
 			Coord2D vector = { 1, 0 };
 			auto start = time::now();
-			for (double_t i = 0; i <= M_PI * 2 + RADIAN_FRACTION; utility::Increment(i, RADIAN_FRACTION))
+			for (double_t i = 0; i <= M_PI * 2 + RADIAN_FRACTION; Increment(i, RADIAN_FRACTION))
 			{
 				func(vector);
-				utility::Rotate(vector, RADIAN_FRACTION);
+				Rotate(vector, RADIAN_FRACTION);
 			}
 			return time::now() - start;
 		};
+		outfile << "# Vector Rotation\n";
 
-		std::cout << "Dot Duration: " << rotationIterator([&](Coord2D const& vector)
-		{ utility::DotAngleH(vector); }).count() << "ns\n";
-		std::cout << "Trig Duration: " << rotationIterator([&](Coord2D const& vector)
-		{ utility::TrigAngleH(vector); }).count() << "ns\n";
+		tableHeader(outfile, "Dot");
+		outfile << "### Duration: " << rotationIterator([&](Coord2D const& vector)
+		{ 
+			outfile << "|"
+				<< " " << cts(vector).c_str() << " |"
+				<< " " << dts(std::acos(DotAngleH(vector))).c_str() << " |\n";
+		}).count() << "ns\n\n";
+
+		tableHeader(outfile, "Trig");
+		outfile << "### Duration: " << rotationIterator([&](Coord2D const& vector)
+		{ 
+			outfile << "|"
+				<< " " << cts(vector).c_str() << " |"
+				<< " " << dts(TrigAngleH(vector)).c_str() << " |\n";
+		}).count() << "ns\n\n";
 	}
 
 	{
@@ -104,21 +146,32 @@ int _tmain(int argc, _TCHAR* argv[])
 			std::chrono::nanoseconds duration;
 
 			auto start = time::now();
-			for (double_t i = 0; i <= (M_PI * 2) + RADIAN_FRACTION; utility::Increment(i, RADIAN_FRACTION))
+			for (double_t i = 0; i <= (M_PI * 2) + RADIAN_FRACTION; Increment(i, RADIAN_FRACTION))
 			{
 				func({ std::cos(i), std::sin(i) });
 			}
 			return time::now() - start;
 		};
 
-		std::cout << "Dot Duration: " << polarIterator([&](Coord2D const& vector)
-		{ utility::DotAngleH(vector); }).count() << "ns\n";
-		std::cout << "Trig Duration: " << polarIterator([&](Coord2D const& vector)
-		{ utility::TrigAngleH(vector); }).count() << "ns\n";
+		outfile << "# Polar Rotation\n";
+
+		tableHeader(outfile, "Dot");
+		outfile << "### Duration: " << polarIterator([&](Coord2D const& vector)
+		{
+			outfile << "|"
+				<< " " << cts(vector).c_str() << " |"
+				<< " " << dts(std::acos(DotAngleH(vector))).c_str() << " |\n";
+		}).count() << "ns\n";
+
+		tableHeader(outfile, "Trig");
+		outfile << "### Duration: " << polarIterator([&](Coord2D const& vector)
+		{
+			outfile << "|"
+				<< " " << cts(vector).c_str() << " |"
+				<< " " << dts(TrigAngleH(vector)).c_str() << " |\n";
+		}).count() << "ns\n";
 	}
 
-	char c;
-	std::cin >> c;
 	//verify against http://products.wolframalpha.com/api/
 	//can use this? http://wolframalphaapi20.codeplex.com/
 	//verify against: http://www.wolframalpha.com/input/?i=atan%280%2C-1%29
