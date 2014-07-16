0d409625e87d55646c5fa515a5faa59bd5684539
brianrackle <brianrackle@hotmail.com>
2014-07-15 17:16:58 -0700

    functionalizing file outputs and vector iterations

--- console/console.cpp
+++ console/console.cpp
@@ -23,21 +23,22 @@ struct Line
 	Coord2D from;
 };
 
+/* math */
 
 //Calculate the angle of the vector to the horizontal.
-__forceinline double_t DotAngleH(Coord2D const& vector)
+__forceinline double_t dot_angle(Coord2D const& vector)
 {
 	return vector.x / std::hypot(vector.x, vector.y);
 }
 
 //Calculate the angle of the vector to the horizontal using atan2.
-__forceinline double_t TrigAngleH(Coord2D const& line)
+__forceinline double_t trig_angle(Coord2D const& line)
 {
 	return 	std::atan2(line.y, line.x);
 }
 
 //Apply rotation matrix to the vector using radians
-__forceinline void Rotate(Coord2D & vector, double_t radians)
+__forceinline void rotate(Coord2D & vector, double_t radians)
 {
 	double_t s = std::sin(radians);
 	double_t c = std::cos(radians);
@@ -45,12 +46,47 @@ __forceinline void Rotate(Coord2D & vector, double_t radians)
 	vector = { vector.x * c - vector.y * s, vector.x * s + vector.y * c };
 }
 
-//Increment lhs by rhs
-__forceinline void Increment(double_t & lhs, double_t const& rhs)
+//increment lhs by rhs
+__forceinline void increment_value(double_t & lhs, double_t const& rhs)
 {
 	lhs += rhs;
 }
 
+/* iteration */
+
+//iterate through all the values between 0 and M_PI * 2 at increment intervals
+//at each interval create a vector at the interval value and pass the vector to func
+std::chrono::nanoseconds rotation_iteration(double_t const increment, std::function<void(Coord2D const&)> const& func)
+{
+	using time = std::chrono::high_resolution_clock;
+	std::chrono::nanoseconds duration;
+
+	Coord2D vector = { 1, 0 };
+	auto start = time::now();
+	for (double_t i = 0; i <= M_PI * 2; increment_value(i, increment))
+	{
+		func(vector);
+		rotate(vector, increment);
+	}
+	return time::now() - start;
+};
+
+//iterate through all the values between 0 and M_PI * 2 at increment intervals
+//at each interval create a vector at the interval value and pass the vector to func
+std::chrono::nanoseconds polar_iteration(double_t const increment, std::function<void(Coord2D const&)> const& func)
+{
+	using time = std::chrono::high_resolution_clock;
+	std::chrono::nanoseconds duration;
+
+	auto start = time::now();
+	for (double_t i = 0; i <= M_PI * 2; increment_value(i, increment))
+		func({ std::cos(i), std::sin(i) });
+	return time::now() - start;
+};
+
+/* string conversion */
+
+//convert double to string
 __forceinline std::string dts(double_t d)
 {
 	char buf[50];
@@ -58,12 +94,14 @@ __forceinline std::string dts(double_t d)
 	return buf;
 }
 
+//convert Coord2d to string
 __forceinline std::string cts(Coord2D const& vector)
 {
 	return dts(vector.x) + " , " + dts(vector.y);
 }
 
-__forceinline void TableHeader(std::ofstream & file, char const* format, char const* header...)
+//create markdown table header
+__forceinline void table_header(std::ofstream & file, char const* format, char const* headers...)
 {
 	va_list args;
 	va_start(args, format);
@@ -90,114 +128,61 @@ __forceinline void TableHeader(std::ofstream & file, char const* format, char co
 	file << "|\n";
 }
 
-#define RADIAN_FRACTION 1.0e-2
+//create markdown table row
+__forceinline void table_row(std::ofstream & file, int const count, char const* items...)
+{
+	for (int i = 0; i < count; ++i)
+		file << "| " << items << " ";
+	file << "|\n";
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//Example 1: Rotating vector with lambda iterator
-	//benefits:
-	//	-versatile, can pass any worker into angleIterator
-	//	-no memory storage
-	//alternatives:
-	//	-populate an std::vector with the values and then iterate. Excessive memory usage
-	
 	std::ofstream outfile("results.markdown");
 	//link TOC so we can put a link back at the beginning of each header
-	outfile << "[TOC]\n";
-	
-	auto tableHeader = [](std::ofstream & file, char const* tableName)
+	outfile << "<a name = \"toc\"></a>"
+		<< "\n\n[TOC]\n\n";
+
+	const double_t increment = 1.0e-2;
+
+	double count;
+	double sum;
+	double mean;
+	double minDelta;
+	double maxDelta;
+
+	outfile << "# Vector Rotation\n";
+
+	outfile << "## " << "Dot Results\n";
+	table_header(outfile, "lr", "Vector", "Radians");
+	outfile << "### Duration: " << rotation_iteration(increment, [&](Coord2D const& vector)
+	{ 
+		table_row(outfile, 2, cts(vector).c_str(), dts(std::acos(dot_angle(vector))).c_str());
+	}).count() << "ns\n\n";
+
+	outfile << "## " << "Trig Results\n";
+	table_header(outfile, "lr", "Vector", "Radians");
+	outfile << "### Duration: " << rotation_iteration(increment, [&](Coord2D const& vector)
+	{ 
+		table_row(outfile, 2, cts(vector).c_str(), dts(trig_angle(vector)).c_str());
+	}).count() << "ns\n\n";
+
+	outfile << "# Polar Rotation\n";
+
+	outfile << "## " << "Dot Results\n";
+	table_header(outfile, "lr", "Vector", "Radians");
+	outfile << "### Duration: " << polar_iteration(increment, [&](Coord2D const& vector)
 	{
-		file << "## " << tableName << " Results\n"
-			<< "| Vector | Radians |\n"
-			<< "|:------ | -------:|\n";
-	};
+		table_row(outfile, 2, cts(vector).c_str(), dts(std::acos(dot_angle(vector))).c_str());
+	}).count() << "ns\n";
 
-	//rotate because error builds up in vector.
-	//vs
-	//vector being recalculated from polar on each iteration
+	outfile << "## " << "Trig Results\n";
+	table_header(outfile, "lr", "Vector", "Radians");
+	outfile << "### Duration: " << polar_iteration(increment, [&](Coord2D const& vector)
 	{
-		double count;
-		double sum;
-		double mean;
-		double minDelta;
-		double maxDelta;
-
-		//could also store delta of every-1 angle and make a 1d heatmap out of it
-		auto rotationIterator =
-			[](std::function<void(Coord2D const&)> func)
-			-> std::chrono::nanoseconds
-		{
-			using time = std::chrono::high_resolution_clock;
-			std::chrono::nanoseconds duration;
-
-			Coord2D vector = { 1, 0 };
-			auto start = time::now();
-			for (double_t i = 0; i <= M_PI * 2 + RADIAN_FRACTION; Increment(i, RADIAN_FRACTION))
-			{
-				func(vector);
-				Rotate(vector, RADIAN_FRACTION);
-			}
-			return time::now() - start;
-		};
-
-		outfile << "# Vector Rotation\n";
-
-		outfile << "## " << "Dot Results\n";
-		TableHeader(outfile, "lr", "Vector", "Radians");
-		outfile << "### Duration: " << rotationIterator([&](Coord2D const& vector)
-		{ 
-			outfile << "|"
-				<< " " << cts(vector).c_str() << " |"
-				<< " " << dts(std::acos(DotAngleH(vector))).c_str() << " |\n";
-		}).count() << "ns\n\n";
-
-		tableHeader(outfile, "Trig");
-		outfile << "### Duration: " << rotationIterator([&](Coord2D const& vector)
-		{ 
-			outfile << "|"
-				<< " " << cts(vector).c_str() << " |"
-				<< " " << dts(TrigAngleH(vector)).c_str() << " |\n";
-		}).count() << "ns\n\n";
-	}
-
-	{
-		auto polarIterator =
-			[](std::function<void(Coord2D const&)> func)
-			-> std::chrono::nanoseconds
-		{
-			using time = std::chrono::high_resolution_clock;
-			std::chrono::nanoseconds duration;
-
-			auto start = time::now();
-			for (double_t i = 0; i <= (M_PI * 2) + RADIAN_FRACTION; Increment(i, RADIAN_FRACTION))
-			{
-				func({ std::cos(i), std::sin(i) });
-			}
-			return time::now() - start;
-		};
-
-		outfile << "# Polar Rotation\n";
-
-		tableHeader(outfile, "Dot");
-		outfile << "### Duration: " << polarIterator([&](Coord2D const& vector)
-		{
-			outfile << "|"
-				<< " " << cts(vector).c_str() << " |"
-				<< " " << dts(std::acos(DotAngleH(vector))).c_str() << " |\n";
-		}).count() << "ns\n";
-
-		tableHeader(outfile, "Trig");
-		outfile << "### Duration: " << polarIterator([&](Coord2D const& vector)
-		{
-			outfile << "|"
-				<< " " << cts(vector).c_str() << " |"
-				<< " " << dts(TrigAngleH(vector)).c_str() << " |\n";
-		}).count() << "ns\n";
-	}
-
-	//verify against http://products.wolframalpha.com/api/
-	//can use this? http://wolframalphaapi20.codeplex.com/
-	//verify against: http://www.wolframalpha.com/input/?i=atan%280%2C-1%29
+		table_row(outfile, 2, cts(vector).c_str(), dts(trig_angle(vector)).c_str());
+	}).count() << "ns\n";
+	outfile << "[back to top](#toc)" << std::endl;
 	return EXIT_SUCCESS;
 }
 
