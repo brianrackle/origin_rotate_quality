66dc0ec970b48353bdd894ecbf281de247977b22
brianrackle <brianrackle@hotmail.com>
2014-10-07 22:42:52 -0700

    switching const and type

--- console/console.cpp
+++ console/console.cpp
@@ -20,38 +20,38 @@ struct coord2d
 
 namespace func_def
 {
-	using theta = std::function < double_t(coord2d const&) >;
-	using clamp = std::function < double_t(double_t const) >;
-	using iterable = std::function < void(coord2d const&, double_t const) >;
-	using iterate = std::function < std::chrono::nanoseconds(double_t const, iterable const&) >;
+	using theta = std::function < double_t(const coord2d &) >;
+	using clamp = std::function < double_t(const double_t) >;
+	using iterable = std::function < void(const coord2d &, const double_t) >;
+	using iterate = std::function < std::chrono::nanoseconds(const double_t, const iterable &) >;
 }
 
 /* math */
 
 //Calculate the theta of the vector to the horizontal.
- double_t dot_theta(coord2d const& vector)
+ double_t dot_theta(const coord2d & vector)
 {
 	 return std::acos(vector.x / std::hypot(vector.x, vector.y));
 }
 
- double_t dot_clamp(double_t const theta)
+ double_t dot_clamp(const double_t theta)
  {
 	 return theta > M_PI ? (2 * M_PI) - theta : theta;	 
  }
 
 //Calculate the theta of the vector to the horizontal using atan2.
- double_t trig_theta(coord2d const& line)
+ double_t trig_theta(const coord2d & line)
 {
 	return 	std::atan2(line.y, line.x);
 }
 
- double_t trig_clamp(double_t const theta)
+ double_t trig_clamp(const double_t theta)
  {
 	 return theta > M_PI ? -((2 * M_PI) - theta) : theta;
  }
 
 //Apply rotation matrix to the vector using theta
-coord2d rotate(coord2d const& vector, double_t const theta)
+coord2d rotate(const coord2d & vector, const double_t theta)
 {
 	double_t s = std::sin(theta);
 	double_t c = std::cos(theta);
@@ -62,7 +62,7 @@ coord2d rotate(coord2d const& vector, double_t const theta)
 /* iterate */
 
 std::chrono::nanoseconds rotation_iteration
-(double_t const increment, func_def::iterable const& iterable)
+(const double_t increment, const func_def::iterable & iterable)
 {
 	using time = std::chrono::high_resolution_clock;
 	std::chrono::nanoseconds duration;
@@ -77,7 +77,7 @@ std::chrono::nanoseconds rotation_iteration
 //iterate through all the values between 0 and M_PI * 2 at increment intervals
 //at each interval create a vector at the interval value and pass the vector to iterable
 std::chrono::nanoseconds rotation_inc_iteration
-(double_t const increment, func_def::iterable const& iterable)
+(const double_t increment, const func_def::iterable & iterable)
 {
 	using time = std::chrono::high_resolution_clock;
 	std::chrono::nanoseconds duration;
@@ -96,7 +96,7 @@ std::chrono::nanoseconds rotation_inc_iteration
 //iterate through all the values between 0 and M_PI * 2 at increment intervals
 //at each interval create a vector at the interval value and pass the vector to iterable
 std::chrono::nanoseconds polar_iteration
-(double_t const increment, func_def::iterable const& iterable)
+(const double_t increment, const func_def::iterable & iterable)
 {
 	using time = std::chrono::high_resolution_clock;
 	std::chrono::nanoseconds duration;
@@ -119,16 +119,16 @@ std::chrono::nanoseconds polar_iteration
 }
 
 //convert coord2d to string
- std::string cts(coord2d const& vector)
+ std::string cts(const coord2d & vector)
 {
 	return dts(vector.x) + " , " + dts(vector.y);
 }
 
-void table_header(std::ofstream & file, char const* format)
+void table_header(std::ofstream & file, const char * format)
 {
 	file << "|\n";
 
-	for (char const* pos = format; *pos; ++pos)
+	for (const char * pos = format; *pos; ++pos)
 		switch (*pos)
 	{
 		case 'l':
@@ -148,7 +148,7 @@ void table_header(std::ofstream & file, char const* format)
 
 //create markdown table header
 template<typename ... Types>
-void table_header(std::ofstream & file, char const* format, char const * first, Types ... rest)
+void table_header(std::ofstream & file, const char * format, const char * first, Types ... rest)
 {
 	file << "| " << first << " ";
 	table_header(file, format, rest...);
@@ -161,14 +161,14 @@ void table_row(std::ofstream & file)
 
 //create markdown table row
 template <typename ... Types>
-void table_row(std::ofstream & file, char const* first, Types ... rest)
+void table_row(std::ofstream & file, const char * first, Types ... rest)
 {
 	file << "| " << first << " ";
 	table_row(file, rest...);
 }
 
 //create markdown heading
-void heading(std::ofstream & file, int const level, char const* head)
+void heading(std::ofstream & file, const int level, const char * head)
 {
 	for (int i = 0; i < level; ++i)
 		file << "#";
@@ -177,14 +177,14 @@ void heading(std::ofstream & file, int const level, char const* head)
 //pass the theta to the func_def::iterate function so that the theta used to calculate the vector can be used for comparison
 //output thetas and stats using iterate functon
 void calculation_block
-(std::ofstream & outfile, double_t const increment, func_def::iterate const& iterate, func_def::theta const& calc_theta, func_def::clamp const& clamp)
+(std::ofstream & outfile, const double_t increment, const func_def::iterate & iterate, const func_def::theta & calc_theta, const func_def::clamp & clamp)
 {
 	size_t count = 0;
 	double_t sumDelta = 0;
 	double_t minDelta = std::numeric_limits<double_t>::max();
 	double_t maxDelta = std::numeric_limits<double_t>::lowest();
 
-	auto duration = iterate(increment, [&](coord2d const& vector, double_t const theta)
+	auto duration = iterate(increment, [&](const coord2d & vector, const double_t theta)
 	{
 		double_t calced_theta = calc_theta(vector);
 		double_t clamped_theta = clamp(theta);
@@ -215,7 +215,7 @@ void calculation_block
 	heading(outfile, 3, ("Max Delta: " + dts(maxDelta)).c_str());
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, _TCHAR * argv[])
 {
 	std::ofstream outfile("results.markdown");
 	
